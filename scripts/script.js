fb86fb5b50cb4c6648ac3e08c3d9d798bf0fb609
julianbooher <julianbooher@hotmail.com>
2020-11-27 21:30:50 -0600

    got some divs up to put project into.

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,6 @@
-/*need to update this to be universal accross all buttons, better designed */
+$(document).ready(onReady);
+console.log('in script.js')
 
-document.querySelector('#mtg-videos-button').onclick = function (){
-    if(document.querySelector('#mtg-videos').style.display === 'none'){
-        document.querySelector('#mtg-videos').style.display ='block';
-    }
-    else{
-        document.querySelector('#mtg-videos').style.display ='none';
-    }
-}
-
-document.querySelector('#soccer-videos-button').onclick = function(){
-    if(document.querySelector('#soccer-videos').style.display === 'none'){
-        document.querySelector('#soccer-videos').style.display ='block';
-    }
-    else{
-        document.querySelector('#soccer-videos').style.display ='none';
-    }
-}
-
-document.querySelector('#film-videos-button').onclick = function(){
-    if(document.querySelector('#film-videos').style.display === 'none'){
-        document.querySelector('#film-videos').style.display ='block';
-    }
-    else{
-        document.querySelector('#film-videos').style.display ='none';
-    }
+function onReady(){
+    console.log('in jQuery');
 }
